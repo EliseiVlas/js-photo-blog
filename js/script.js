b77374b27dd2b91da7e893503cab96e82d1d243d
Elisei <eliseivlas.002@gmail.com>
2025-01-24 14:04:58 +0100

    consegna esercizio senza bonus finale

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// selezioniamo gli elementi di output
+const card = document.querySelector('.contenitore')
+console.log(card);
+
+// setto l'endpoint
+
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+
+// faccio partire la richiesta Ajax verso lAPI per ricevere i dati
+axios.get(endpoint)
+            //codice da eseguire in caso di successo
+            .then(responseObj => {
+                
+                const posts = responseObj.data;
+
+                for(let i = 0; i < posts.length; i++) {
+                    let post = posts[i];
+                    console.log(post);
+                    
+                    const  {title, date, url } = post
+
+                    card.innerHTML +=`
+            <div class="card">
+                <div class="card-content">
+                    <img src="${url}" alt="">
+                    <span class="data">${date}</span>
+                    <h2>${title.toUpperCase()}</h2>
+                </div>
+                <div class="pin">
+                    <img src="./img/pin.svg" alt="">
+                </div>
+            </div>
+                    `
+                }
+                
+            })
+            // codice da eseguire in caso di errore
+            .catch(error => {
+                
+                console.error(error)
+            })
